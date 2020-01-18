7e31f497da25eb1f7d3996dcfc97cdb74a7cc533
廖 <lsp@nongline.com>
2020-01-18 14:43:06 +0800

    3

---
+++ README.md
@@ -0,0 +1,83 @@
+<h1 align="center">A simple  Array search and array paging </h1>
+
+<p align="center"> .</p>
+
+
+## Installing
+
+```shell
+$ composer require liaosp/arraySearch -vvv
+```
+
+## Usage
+
+example：
+
+```php
+$target = [
+    [
+        'id'=>1,
+        'status'=>3
+        ],
+    [
+        'id'=>2,
+        'status'=>1
+    ],
+    [
+        'id'=>3,
+        'status'=>2
+    ],
+    [
+        'id'=>4,
+        'status'=>1
+    ],
+    [
+        'id'=>5,
+        'status'=>1
+    ],
+    [
+        'id'=>6,
+        'status'=>1
+    ],
+
+];
+```
+
+```
+$obj = new ArraySearch();
+
+$data = $obj->arrayData($target)->paginate(1);//firstPageData  ，第一页数据。
+
+```
+
+where：
+
+```
+$data = $obj->arrayData($target)->where(['status'=>1])->paginate(1);//firstPageData  ，第一页数据。
+```
+whereIn:
+
+```
+$data = $obj->arrayData($target)->whereIn(['status'=>[1,2]])->paginate(1);//firstPageData  ，第一页数据。
+```
+
+orWhere:
+
+```
+$data = $obj->arrayData($target)->orWhere(['status'=>1,'id'=>1])->paginate(1);//firstPageData  ，第一页数据。
+```
+
+
+## Contributing
+
+You can contribute in one of three ways:
+
+1. File bug reports using the [issue tracker](https://github.com/liaoshengping/arraySearch/issues).
+2. Answer questions or fix bugs on the [issue tracker](https://github.com/liaoshengping/arraySearch/issues).
+3. Contribute new features or update the wiki.
+
+_The code contribution process is not very formal. You just need to make sure that you follow the PSR-0, PSR-1, and PSR-2 coding guidelines. Any new code contributions must be accompanied by unit tests where applicable._
+
+## License
+
+MIT
---
+++ src/ArraySearch.php
@@ -0,0 +1,156 @@
+<?php
+
+
+namespace Liaosp\ArraySearch;
+
+
+use Crasphb\Pagination;
+
+class ArraySearch{
+    /**
+     * @var array
+     */
+    public $where=[];
+    /**
+     * @var array
+     */
+    public $orWhere=[];
+    /**
+     * @var int
+     */
+    public $page=1;
+    /**
+     * @var int
+     */
+    public $pageSize=2;
+    /**
+     * @var array
+     */
+    public $resArray = array();//查询的结果
+    /**
+     * @var
+     */
+    public $arrayData;//最开始的数组，二维数组
+    /**
+     * @var array
+     */
+    public $whereIn = array();
+
+    public $pageConfig  = ['style' => 1,'simple'=>false,'allCounts'=>true,'nowAllPage'=>true,'toPage'=>true];
+
+    /**
+     * @param array $arrayData
+     * @return $this
+     */
+    public function arrayData(array $arrayData){
+        $this->arrayData = $arrayData;
+        return $this;
+    }
+    /**
+     *
+     * @param array $where ['status'=>1,'status'=>2,'id'=>'1']
+     * @return \ArraySearch
+     */
+    public function where(array $where){
+        $this->where = array_merge($where,$this->where);
+        return $this;
+    }
+
+    /**
+     * @param array $where
+     * @return $this
+     */
+    public function orWhere(array $where){
+        $this->orWhere = array_merge($where,$this->orWhere);
+        return $this;
+    }
+
+    /**
+     * @param array $where ['status'=>[1,2,3]]
+     * @return $this
+     */
+    public function whereIn(array $where){
+        $this->whereIn = array_merge($where,$this->whereIn);
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     * @throws \Exception
+     */
+    public function get(){
+        $this->common();
+        return $this->arrayData;
+    }
+
+    /**
+     * 獲取分頁
+     * @param int $page
+     * @return mixed
+     * @throws \Exception
+     */
+    public function paginate($page=1){
+        $this->page = $page;
+        $this->common();
+        $obj =new Pagination($this->arrayData,$this->pageSize,$this->pageConfig);
+        $obj->pageNow = $page;
+        $this->arrayData =$obj->getItem();
+        return $this->arrayData;
+    }
+
+    /**
+     * 获取第三方扩展参数
+     * @return  Pagination
+     */
+    public function Pagination(){
+
+        $obj =new Pagination($this->arrayData,$this->pageSize,$this->pageConfig);
+        $obj->pageNow = $this->page;
+        return $obj;
+    }
+
+    /**
+     * 公共处理
+     * @throws \Exception
+     */
+    protected function common(){
+        if(empty($this->arrayData)){
+            throw new \Exception('数据为空，请调用arrayData 方法初始化数据');
+        }
+        if(!empty($this->where)){
+            $this->arrayData = array_filter($this->arrayData, function($var){
+                $bool = true;
+                foreach ($this->where as $key=>$orwehre){
+                    if($var[$key] != $orwehre){
+                        $bool = false;
+                    }
+                }
+                return $bool;
+            });
+        }
+        if(!empty($this->whereIn)){
+            $this->arrayData = array_filter( $this->arrayData, function($var){
+                $bool = false;
+                foreach ($this->whereIn as $key=>$orwehre){
+                    if( in_array($var[$key],$orwehre)){
+                        return true;
+                        break;
+                    }
+                }
+                return $bool;
+            });
+        }
+        if(!empty($this->orWhere)){
+            $this->arrayData = array_filter( $this->arrayData, function($var){
+                $bool = false;
+                foreach ($this->orWhere as $key=>$orwehre){
+                    if($var[$key] == $orwehre){
+                        return true;
+                        break;
+                    }
+                }
+                return $bool;
+            });
+        }
+    }
+}
